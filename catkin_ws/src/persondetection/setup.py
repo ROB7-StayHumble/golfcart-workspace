aee7c2f106decf3c88ecb03b5a3a27f2f73f7644
glhr <grenblot@gmail.com>
2019-11-21 08:21:08 +0100

    integrating yolov3 in person detection node

--- catkin_ws/src/persondetection/setup.py
+++ catkin_ws/src/persondetection/setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['persondetection'],
+    packages=['persondetection','yolov3','yolov3.utils'],
     package_dir={'': 'include'},
 )
 setup(**setup_args)
