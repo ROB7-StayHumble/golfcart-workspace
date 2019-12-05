c5cfc0a890aeaf10e1d22aba5cbd55c8e4ee872c
glhr <grenblot@gmail.com>
2019-12-05 11:29:31 +0100

    plot confidence map for ZED image

--- catkin_ws/src/persondetection/src/testing/testing_yoloconfmap.py
+++ catkin_ws/src/persondetection/src/testing/testing_yoloconfmap.py
@@ -6,32 +6,61 @@ from utils.transformbox import get_boxes_zedframe
 from persondetection.run_yolo import *
 
 SHOW_PLOTS = True
+cam = "IR"
 
 folder = "src/persondetection/src/testing/"
 
 print(os.getcwd())
-for imgpath in glob.glob(folder+"test_img/ircam*.png"):
-    #thresholding(imgpath)
-    print(imgpath)
-    img = cv2.imread(imgpath)
+if cam == "IR":
+    for imgpath in glob.glob(folder+"test_img/ircam*.png"):
+        #thresholding(imgpath)
+        print(imgpath)
+        img = cv2.imread(imgpath)
 
-    cimg, boxes = detect_from_img(img)
-    boxes_zedframe = get_boxes_zedframe([Box(blank_ir_3D,xyxy=box['coords'],confidence=1) for box in boxes])
-    boxes_img = plot_boxes_on_img(blank_zed_3D,[box.xyxy for box in boxes_zedframe])
-    map = makeConfidenceMapFromBoxes(blank_zed_3D, boxes_zedframe)
+        cimg, boxes = detect_from_img(img)
+        boxes_zedframe = get_boxes_zedframe([Box(blank_ir_3D,xyxy=box['coords'],confidence=1) for box in boxes])
+        boxes_img = plot_boxes_on_img(blank_zed_3D,[box.xyxy for box in boxes_zedframe])
+        map = makeConfidenceMapFromBoxes(blank_zed_3D, boxes_zedframe)
 
-    images = [cimg, boxes_img, map]
-    titles = ["YOLOv3-tiny detection","Perspective transformation to\nZED camera frame", "Confidence map"]
-    fig = plt.figure(num=None, figsize=(12, 4), dpi=300)
+        images = [cimg, boxes_img, map]
+        titles = ["YOLOv3-tiny detection","Perspective transformation to\nZED camera frame", "Confidence map"]
+        fig = plt.figure(num=None, figsize=(12, 4), dpi=300)
 
-    plt.rcParams["axes.titlesize"] = 4
-    for i in range(len(images)):
-        plt.subplot(1, len(images), i + 1), plt.imshow(images[i], 'gray')
-        plt.title(titles[i])
-        plt.xticks([]), plt.yticks([])
+        plt.rcParams["axes.titlesize"] = 4
+        for i in range(len(images)):
+            plt.subplot(1, len(images), i + 1), plt.imshow(images[i], 'gray')
+            plt.title(titles[i])
+            plt.xticks([]), plt.yticks([])
 
-    fig.tight_layout()
-    # plt.imshow(map)
+        fig.tight_layout()
+        # plt.imshow(map)
 
-    if SHOW_PLOTS: plt.show()
-    # histo(imgpath)
+        if SHOW_PLOTS: plt.show()
+        # histo(imgpath)
+elif cam == "ZED":
+    for imgpath in glob.glob(folder + "test_img/zedcam*.png"):
+        # thresholding(imgpath)
+        print(imgpath)
+        img = cv2.imread(imgpath)
+
+        cimg, boxes = detect_from_img(img)
+        boxes_class = [Box(blank_zed_3D, xyxy=box['coords'], confidence=1) for box in boxes]
+        # boxes_img = plot_boxes_on_img(blank_ir_3D, [box.xyxy for box in boxes_class])
+        map = makeConfidenceMapFromBoxes(blank_zed_3D, boxes_class)
+
+        cimg = cv2.cvtColor(cimg, cv2.COLOR_BGR2RGB)
+        images = [cimg, map]
+        titles = ["YOLOv3-tiny detection", "Confidence map"]
+        fig = plt.figure(num=None, figsize=(12, 4), dpi=300)
+
+        plt.rcParams["axes.titlesize"] = 4
+        for i in range(len(images)):
+            plt.subplot(1, len(images), i + 1), plt.imshow(images[i], 'gray')
+            plt.title(titles[i])
+            plt.xticks([]), plt.yticks([])
+
+        fig.tight_layout()
+        # plt.imshow(map)
+
+        if SHOW_PLOTS: plt.show()
+        # histo(imgpath)
