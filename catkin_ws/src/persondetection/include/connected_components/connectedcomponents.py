fixed score calculation error

catkin_ws/src/persondetection/include/connected_components/connectedcomponents.py
@@ -21,7 +21,9 @@ def distance_from_line(x,y,a,b):
     diff = np.abs(y - predicted_y)
     return diff
 
-def calculate_confidence_score(box_x,box_y,model_slope=SLOPE['y_height'],model_intercept=INTERCEPT['y_height']):
+def calculate_confidence_score(box_x,box_y,model):
+    model_slope = SLOPE[model]
+    model_intercept = INTERCEPT[model]
 
     diff_max = np.max([ distance_from_line(0,0,model_slope,model_intercept),
                         distance_from_line(0,1,model_slope,model_intercept),
@@ -29,6 +31,8 @@ def calculate_confidence_score(box_x,box_y,model_slope=SLOPE['y_height'],model_i
                         distance_from_line(1,0,model_slope,model_intercept)])
     diff = distance_from_line(box_x,box_y,model_slope,model_intercept)
     score = np.power(1 - diff/(diff_max),2)
+
+    # score = 1 - diff / (diff_max)
     #print(diff)
     #print(box_x,box_y,model_slope,model_intercept,score)
     return np.round(score,decimals=2)

catkin_ws/src/persondetection/include/utils/boxes.py
@@ -40,6 +40,7 @@ class Box:
                                  'y':xyxy[3]}
             self.h = xyxy[3] - xyxy[1]
             self.w = xyxy[2] - xyxy[0]
+
             self.x = xyxy[0] + self.w/2
             self.y = xyxy[1] + self.h/2
 
@@ -53,8 +54,8 @@ class Box:
         rel_y = self.y/self.img_h
         rel_x = self.x/self.img_w
 
-        score_yh = np.round(calculate_confidence_score(rel_y, rel_h),decimals=2)
-        score_aspect_ratio = np.round(calculate_confidence_score(rel_h, rel_w),decimals=2)
+        score_yh = calculate_confidence_score(rel_y, rel_h, model='y_height')
+        score_aspect_ratio = calculate_confidence_score(rel_h, rel_w, model='aspect_ratio')
         score_dimensions = np.round(score_yh * score_aspect_ratio,decimals=2)
         total_score = score_dimensions
 

catkin_ws/src/persondetection/src/testing/testing_confidencescore_example.py
@@ -24,8 +24,8 @@ def plot_box_with_score(img,h,w,x,y):
     rel_x = x / img_w
     print(rel_h, rel_w, rel_x, rel_y)
 
-    score_yh = calculate_confidence_score(rel_y, rel_h)
-    score_aspect_ratio = calculate_confidence_score(rel_h, rel_w)
+    score_yh = calculate_confidence_score(rel_y, rel_h, model='y_height')
+    score_aspect_ratio = calculate_confidence_score(rel_h, rel_w, model='aspect_ratio')
     score_dimensions = np.round(np.round(score_yh, decimals=2) * np.round(score_aspect_ratio, decimals=2), decimals=2)
     if score_dimensions > 0.5: color = GREEN
     else: color = RED

catkin_ws/src/persondetection/src/testing/testing_confmap_eval.py
@@ -11,40 +11,60 @@ cam = "IR"
 
 folder = "src/persondetection/src/testing/"
 
-pairs = [("1571746625368332498_ircam.png","1571746625385033721_zed.png")]
+pairs = [("1571746625368332498_ircam.png","1571746625385033721_zed.png","1571746625385033721_GT.png"),
+         ("1571746631630210616_ircam.png","1571746631605020001_zed.png","1571746631605020001_GT.png"),
+         ("1571746633060068411_ircam.png","1571746633070918853_zed.png","1571746633070918853_GT.png")]
 
 for pair in pairs:
     ir = cv2.imread(folder+"test_img/ir/"+pair[0])
     ir_1channel = cv2.imread(folder + "test_img/ir/" + pair[0],0)
     zed = cv2.imread(folder + "test_img/zed/" + pair[1])
 
+    gt = cv2.imread(folder + "test_img/gt/" + pair[2])
+
     ir_cc_img, ir_cc_boxes = detect_connected_components_updated(ir_1channel)
     ir_yolo_img, ir_yolo_boxes = detect_from_img(ir)
     zed_yolo_img, zed_yolo_boxes = detect_from_img(zed)
 
-    ir_yolo_boxes_zedframe = get_boxes_zedframe([Box(blank_ir_3D, xyxy=box['coords'], confidence=1) for box in ir_yolo_boxes])
-    ir_cc_boxes_zedframe = get_boxes_zedframe([Box(blank_ir_3D, xyxy=box, confidence=1) for box in ir_cc_boxes])
+    ir_cc_boxes_zedframe = get_boxes_zedframe(
+        [Box(blank_ir_3D, xyxy=box, confidence=1) for box in ir_cc_boxes])
     zed_yolo_boxes_zedframe = [Box(blank_zed_3D, xyxy=box['coords'], confidence=1) for box in zed_yolo_boxes]
+    ir_yolo_boxes_zedframe = get_boxes_zedframe(
+        [Box(blank_ir_3D, xyxy=box['coords'], confidence=1) for box in ir_yolo_boxes])
 
     ir_yolo_map = makeConfidenceMapFromBoxes(blank_zed_3D, ir_yolo_boxes_zedframe)
     ir_cc_map = makeConfidenceMapFromBoxes(blank_zed_3D, ir_cc_boxes_zedframe)
     zed_yolo_map = makeConfidenceMapFromBoxes(blank_zed_3D, zed_yolo_boxes_zedframe)
 
-    total_map = (ir_yolo_map + ir_cc_map + zed_yolo_map)/3
+    # total_map = (ir_yolo_map + ir_cc_map + zed_yolo_map)/3
+    ir_yolo_map[np.bitwise_and(ir_cc_map > 0,ir_yolo_map > 0)] = 1
+    total_map = np.maximum.reduce([ir_yolo_map, ir_cc_map, zed_yolo_map])
 
+    zed_yolo_img = cv2.cvtColor(zed_yolo_img, cv2.COLOR_BGR2RGB)
     images = [ir_cc_img,ir_yolo_img,zed_yolo_img,
               ir_cc_map,ir_yolo_map,zed_yolo_map]
-    titles = ["YOLOv3-tiny detection", "Confidence map"]
+    titles = ["Connected components on IR image","YOLOv3-tiny on IR image", "YOLOv3-tiny on RGB image"]
     fig = plt.figure(num=None, figsize=(12, 4), dpi=300)
 
-    plt.rcParams["axes.titlesize"] = 4
+    plt.rcParams["axes.titlesize"] = 6
     for i in range(len(images)):
         plt.subplot(2, len(images)/2, i + 1), plt.imshow(images[i], 'gray')
-        # plt.title(titles[i])
+        if i < len(titles):
+            plt.title(titles[i])
         plt.xticks([]), plt.yticks([])
-    plt.show()
 
+    filename = pair[1].split(".")[-2]
+    # plt.savefig(folder + "results/total_confmap/" + filename + "_detail.png", bbox_inches='tight')
+
+    images = [gt,total_map]
+    titles = ["Ground truth", "Total confidence map"]
     fig = plt.figure(num=None, figsize=(12, 4), dpi=300)
-    plt.imshow(total_map, 'gray')
-    plt.xticks([]), plt.yticks([])
+
+    plt.rcParams["axes.titlesize"] = 8
+    for i in range(len(images)):
+        plt.subplot(1, len(images), i + 1), plt.imshow(images[i], 'gray')
+        plt.title(titles[i])
+        plt.xticks([]), plt.yticks([])
+
+    # plt.savefig(folder + "results/total_confmap/"+filename+"_total.png", bbox_inches='tight')
     plt.show()