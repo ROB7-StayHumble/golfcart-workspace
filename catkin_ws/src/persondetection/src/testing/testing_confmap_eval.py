987e36b55817768a750622888e54f2541f2f455c
glhr <grenblot@gmail.com>
2019-12-10 13:58:08 +0100

    plot PR curve dynamically

--- catkin_ws/src/persondetection/src/testing/testing_confmap_eval.py
+++ catkin_ws/src/persondetection/src/testing/testing_confmap_eval.py
@@ -46,12 +46,23 @@ precision_avg = {}
 recalls = {}
 recall_avg = {}
 
-modes = ["combo", "ir+zed", "zed", "ir"]
+modes = ["combo max", "combo sum", "zed", "ir+zed", "zed", "ir"]
+thresholds = np.linspace(0,1,num=50,endpoint=False)
 
 for mode in modes:
-    IOUs[mode] = []
-    precisions[mode] = []
-    recalls[mode] = []
+    IOUs[mode] = {}
+    precisions[mode] = {}
+    recalls[mode] = {}
+    IOU_avg[mode] = {}
+    precision_avg[mode] = {}
+    recall_avg[mode] = {}
+    for threshold in thresholds:
+        IOUs[mode][str(threshold)] = []
+        precisions[mode][str(threshold)] = []
+        recalls[mode][str(threshold)] = []
+        IOU_avg[mode][str(threshold)] = []
+        precision_avg[mode][str(threshold)] = []
+        recall_avg[mode][str(threshold)] = []
 
 for i,pair in enumerate(pairs):
     if i < 500:
@@ -78,12 +89,16 @@ for i,pair in enumerate(pairs):
 
         for mode in modes:
 
-            if mode == "combo":
+            if mode.startswith("combo"):
                 # total_map = (ir_yolo_map + ir_cc_map + zed_yolo_map)/3
                 # ir_yolo_map[np.bitwise_and(ir_cc_map > 0,ir_yolo_map > 0)] = 1
                 total_map_max = np.maximum.reduce([ir_yolo_map, ir_cc_map, zed_yolo_map])
                 total_map_sum = (ir_yolo_map + ir_cc_map + zed_yolo_map)/3
-                total_map = total_map_max
+
+                if mode == "combo max":
+                    total_map = total_map_max
+                elif mode == "combo sum":
+                    total_map = total_map_sum
 
                 zed_yolo_img = cv2.cvtColor(zed_yolo_img, cv2.COLOR_BGR2RGB)
 
@@ -116,43 +131,46 @@ for i,pair in enumerate(pairs):
                     plt.savefig(folder + "results/total_confmap/"+filename+"_total.png", bbox_inches='tight')
                     # plt.show()
 
-                threshold = 0.8
+                # threshold = 0.45
 
             elif mode == "ir+zed":
                 total_map_max = np.maximum.reduce([ir_yolo_map, zed_yolo_map])
                 total_map_sum = (ir_yolo_map + zed_yolo_map)/2
                 total_map = total_map_max
-                threshold = 0
+                # threshold = 0
 
             elif mode == "zed":
                 total_map = zed_yolo_map
-                threshold = 0
+                # threshold = 0
 
             elif mode == "ir":
                 total_map = ir_yolo_map
-                threshold = 0
+                # threshold = 0
 
             else: continue
 
-            total_map_bin = total_map
-            total_map_bin[total_map > threshold] = 1
-            total_map_bin[total_map <= threshold] = 0
-            gt_intersection = np.bitwise_and(total_map_bin > 0, gt > 0)
-            gt_union = np.bitwise_or(total_map_bin > 0, gt > 0)
-            iou = np.sum(gt_intersection)/np.sum(gt_union)
-            IOUs[mode].append(iou)
 
-            TP = np.sum(gt_intersection)
-            FP = np.sum(np.bitwise_and(total_map_bin > 0, gt == 0))
-            FN = np.sum(np.bitwise_and(total_map_bin == 0, gt > 0))
-            if TP+FP > 0:
-                precision = TP/(TP+FP)
-            else:
-                precision = 0
-            recall = TP/(TP+FN)
+            for threshold in thresholds:
+                total_map_bin = np.float32(total_map)
+                total_map_bin[np.float32(total_map) > threshold] = 1
+                total_map_bin[np.float32(total_map) <= threshold] = 0
+                gt_intersection = np.bitwise_and(total_map_bin > 0, gt > 0)
+                gt_union = np.bitwise_or(total_map_bin > 0, gt > 0)
+                iou = np.sum(gt_intersection)/np.sum(gt_union)
 
-            precisions[mode].append(precision)
-            recalls[mode].append(recall)
+                TP = np.sum(gt_intersection)
+                FP = np.sum(np.bitwise_and(total_map_bin > 0, gt == 0))
+                FN = np.sum(np.bitwise_and(total_map_bin == 0, gt > 0))
+                if TP+FP > 0:
+                    precision = TP/(TP+FP)
+                else:
+                    precision = 0
+                recall = TP/(TP+FN)
+
+                # print(threshold,recall)
+                IOUs[mode][str(threshold)].append(iou)
+                precisions[mode][str(threshold)].append(precision)
+                recalls[mode][str(threshold)].append(recall)
 
             if SAVE_PLOTS or SHOW_PLOTS:
                 images = [gt, total_map_bin, np.float32(gt_intersection)]
@@ -174,20 +192,41 @@ for i,pair in enumerate(pairs):
 
 # print(IOUs)
 
-for key, val in IOUs.items():
-    IOU_avg[key] = np.average(val)
+data = []
 
-print("----- IOU -----\n", IOU_avg)
+for mode in modes:
+    for threshold in thresholds:
+        precision_avg[mode][str(threshold)] = np.average(precisions[mode][str(threshold)])
+        IOU_avg[mode][str(threshold)] = np.average(IOUs[mode][str(threshold)])
+        recall_avg[mode][str(threshold)] = np.average(recalls[mode][str(threshold)])
+        data.append({'precision': precision_avg[mode][str(threshold)],
+         'recall': recall_avg[mode][str(threshold)],
+         'label': mode})
 
-for key, val in precisions.items():
-    precision_avg[key] = np.average(val)
+# print(IOU_avg['combo'],precision_avg['combo'],recall_avg['combo'])
 
-print("----- Precision -----\n", precision_avg)
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
-for key, val in recalls.items():
-    recall_avg[key] = np.average(val)
+df = pd.DataFrame(data)
+print(df)
 
-print("----- Recall -----\n", recall_avg)
+sns.lineplot(x="recall", y="precision", hue="label", data=df)
+plt.show()
+
+
+# print("----- IOU -----\n", IOU_avg)
+#
+# for key, val in precisions['combo']['0.4'].items():
+#     precision_avg[key] = np.average(val)
+#
+# print("----- Precision -----\n", precision_avg)
+#
+# for key, val in recalls['combo']['0.4'].items():
+#     recall_avg[key] = np.average(val)
+#
+# print("----- Recall -----\n", recall_avg)
 
 
 
