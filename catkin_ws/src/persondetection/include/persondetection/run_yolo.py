7864055e9c3f1012b4bc8c0860e127faae3d11cb
glhr <grenblot@gmail.com>
2019-12-21 22:21:23 +0100

    latest

--- catkin_ws/src/persondetection/include/persondetection/run_yolo.py
+++ catkin_ws/src/persondetection/include/persondetection/run_yolo.py
@@ -5,7 +5,7 @@ from yolov3.models import *  # set ONNX_EXPORT in models.py
 from yolov3.utils.datasets import *
 from yolov3.utils.utils import *
 
-folder = '/home/nemo/Documents/rob7/golfcart-workspace/catkin_ws/src/persondetection/include/yolov3/'
+folder = '/home/zacefron/Desktop/golfcart-workspace/catkin_ws/src/persondetection/include/yolov3/'
 CFG =       folder + 'cfg/yolov3-tiny.cfg'
 WEIGHTS =   folder + 'weights/yolov3-tiny.weights'
 SOURCE =    folder + 'data/samples'
@@ -138,7 +138,9 @@ def detect_from_folder(save_txt=False, save_img=False):
 
         print('Done. (%.3fs)' % (time.time() - t0))
 
-def detect_from_img(img):
+def detect_from_img(img, conf=None):
+    if conf is None:
+        conf = CONF_THRESH
     print("Running detection")
     with torch.no_grad():
         img_size = 416
@@ -201,7 +203,7 @@ def detect_from_img(img):
             pred = pred.float()
 
         # Apply NMS
-        pred = non_max_suppression(pred, CONF_THRESH, NMS_THRESH)
+        pred = non_max_suppression(pred, conf, NMS_THRESH)
 
         boxes = []
         # Process detections
